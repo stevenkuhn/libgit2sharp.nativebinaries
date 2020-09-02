9ceeb23e0b8bd177ac156748972d2a05cc4083e1
Steven Kuhn <email@stevenkuhn.net>
2020-09-02 18:25:40 -0500

    Reenable copying files from docker container

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -8,6 +8,6 @@ docker build -t $RID -f Dockerfile.$RID .
 
 docker run -t -e RID=$RID --name=$RID $RID
 
-# docker cp $RID:/nativebinaries/nuget.package/runtimes nuget.package
+docker cp $RID:/nativebinaries/nuget.package/runtimes nuget.package
 
-# docker rm $RID
+docker rm $RID

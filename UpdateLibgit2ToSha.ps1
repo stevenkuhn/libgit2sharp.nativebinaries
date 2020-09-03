feccbc1c7eaa83703813af8c5a6e28707f87b805
Steven Kuhn <email@stevenkuhn.net>
2020-09-02 19:30:45 -0500

    Add creating NuGet package

--- UpdateLibgit2ToSha.ps1
+++ UpdateLibgit2ToSha.ps1
@@ -123,7 +123,7 @@ Push-Location $libgit2Directory
     $dllConfig = @"
 <configuration>
     <dllmap os="linux" cpu="x86-64" wordsize="64" dll="$binaryFilename" target="lib/linux-x64/lib$binaryFilename.so" />
-    <dllmap os="osx" cpu="x86,x86-64" dll="$binaryFilename" target="lib/osx/lib$binaryFilename.dylib" />
+    <dllmap os="osx" cpu="x86-64" dll="$binaryFilename" target="lib/osx/lib$binaryFilename.dylib" />
 </configuration>
 "@
 
